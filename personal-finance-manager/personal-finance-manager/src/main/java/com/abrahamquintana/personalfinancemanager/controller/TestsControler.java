8ef7a37eeffd6d5bdc50e7449f598638fd2b3037
Abraham Quintana <abrahamqm87@gmail.com>
2026-03-22 14:22:26 +0000

    Modify uri

--- personal-finance-manager/personal-finance-manager/src/main/java/com/abrahamquintana/personalfinancemanager/controller/TestsControler.java
+++ personal-finance-manager/personal-finance-manager/src/main/java/com/abrahamquintana/personalfinancemanager/controller/TestsControler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@RestController
+@RestController("/test")
 public class TestsControler {
     private final UserRepository userRepository;
     private final CategoryRepository categoryRepository;
