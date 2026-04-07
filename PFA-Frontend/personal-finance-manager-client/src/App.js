61ef36a2140432efbc7511ce4dd206173f95b275
abraham.quintana <abraham.quintana@bitboxonline.com>
2026-04-07 08:23:51 +0100

    Add initial state: - components Footer & Header - global css file styles.css

--- PFA-Frontend/personal-finance-manager-client/src/App.js
+++ PFA-Frontend/personal-finance-manager-client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="content">
+        <h2>Welcome to Personal Financial Management App</h2>
+        <p>Track your expenses, manage your budget, and achieve your financial goals.</p>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ PFA-Frontend/personal-finance-manager-client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <header className="app-header">
+        <h1>Personal Financial Management</h1>
+        <h3>This is Header</h3>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
